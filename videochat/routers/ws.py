245247a7b8f6d66fd05b3aafd083acf200728a76
ride90 <olegpshenichniy@gmail.com>
2020-10-04 18:32:27 +0200

    WS connnection manager. Improve ws handle.

--- videochat/routers/ws.py
+++ videochat/routers/ws.py
@@ -1,16 +1,73 @@
-from fastapi import APIRouter, WebSocket
+import json
+from typing import List, Tuple, Type, Any, Dict
+
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+
+import utils
 
 
 router = APIRouter()
 
 
+class ConnectionManager:
+    def __init__(self) -> None:
+        self._connections: List[WebSocket] = []
+
+    async def connect(self, ws: WebSocket) -> None:
+        await ws.accept()
+        self._connections.append(ws)
+
+    def disconnect(self, ws: WebSocket) -> None:
+        self._connections.remove(ws)
+
+    async def send_text(self, ws: WebSocket, data: str) -> None:
+        await ws.send_text(data)
+
+    async def send_json(self, ws: WebSocket, data: Dict) -> None:
+        await ws.send_json(data, mode='text')
+
+    async def send_bytes(self, ws: WebSocket, data: bytes) -> None:
+        await ws.send_bytes(data)
+
+    async def receive(self, ws: WebSocket) -> Tuple[Type, Any]:
+        message = await ws.receive()
+        ws._raise_on_disconnect(message)
+
+        # binary message
+        if message.get('bytes'):
+            return (bytes, message['bytes'])
+
+        try:
+            # let's try to decode text as jsonTYPE
+            return (dict, json.loads(message['text']))
+        except json.JSONDecodeError:
+            # nope it's just a text
+            return (str, message['text'])
+
+
+connection_manager: ConnectionManager = ConnectionManager()
+
+
 @router.websocket("/ws")
 async def ws_handler(websocket: WebSocket):
-    await websocket.accept()
+    storage = websocket.app._rooms
+    print('STORAGE', storage)
 
+    await connection_manager.connect(websocket)
     print('[WS] New connection is created')
 
-    while True:
-        message = await websocket.receive_text()
-        print('!!!!!!!!', message)
-        # await websocket.send_text(f"Message text was: {data}")
+    try:
+        while True:
+            _type, data = await connection_manager.receive(websocket)
+            # join the room
+            if _type is dict and data.get('room_id') and data.get('action') == 'join':
+                room_id = data['room_id']
+                peer = utils.add_peer(room_id, websocket, storage)
+                if not peer:
+                    print(f'[WS] Can not add a peer, no room with id: {room_id}')
+                    await websocket.close()
+                await connection_manager.send_json(websocket, {'status': 'joined'})
+
+    except (WebSocketDisconnect, Exception) as e:
+        connection_manager.disconnect(websocket)
+        print(f'[WS] Connection is closed due to: {e}')
--- videochat/utils.py
+++ videochat/utils.py
@@ -1,9 +1,12 @@
+from typing import Any, Dict, Union
 from datetime import datetime, timedelta
+
+from fastapi import WebSocket
 
 import settings
 
 
-def add_room(room_id, storage):
+def add_room(room_id: str, storage: Any) -> Dict:
     room = {
         'id': room_id,
         'expiry': datetime.now() + timedelta(hours=settings.ROOM_EXPIRATION_HOURS),
@@ -13,5 +16,38 @@ def add_room(room_id, storage):
     return storage['rooms'][room_id]
 
 
-def get_room(room_id, storage):
+def get_room(room_id: str, storage: Any) -> Union[Dict, None]:
     return storage.setdefault('rooms', {}).get(room_id)
+
+
+def remove_room(room_id: str, storage: Any) -> None:
+    try:
+        del storage['rooms'][room_id]
+    except KeyError:
+        pass
+
+
+def add_peer(room_id: str, peer: WebSocket, storage: Any) -> Union[Dict, None]:
+    room = get_room(room_id, storage)
+    if not room:
+        return None
+    peer_id = id(peer)
+    room['peers'][peer_id] = {
+        'ws': peer,
+        'id': peer_id
+    }
+    return room['peers'][peer_id]
+
+
+def get_peer(room_id: str, peer_id: int, storage: Any) -> Union[Dict, None]:
+    try:
+        return storage['rooms'][room_id]['peers'][peer_id]
+    except KeyError:
+        return None
+
+
+def remove_peer(room_id: str, peer: WebSocket, storage: Any) -> None:
+    try:
+        del storage['rooms'][room_id]['peers'][id(peer)]
+    except KeyError:
+        pass
